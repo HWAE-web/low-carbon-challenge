d930eb3a058a6ec0e87064a2375bc7c4671fafe9
Haewonny <hawon2205@ewhain.net>
2022-08-22 00:10:31 +0900

    feat: add Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Main from './Components/Main/Main';
+import Map from './Components/Map/Map';
+import Post from './Components/Post/Post';
+import MyPage from './Components/MyPage/MyPage';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Routes>
+      <Route path='/' element={<Main/>}/>
+      <Route path='/map' element={<Map/>}/>
+      <Route path='/post' element={<Post/>}/>
+      <Route path='/mypage' element={<MyPage/>}/>
+    </Routes>
   );
 }
 
---
+++ src/fonts/font.js
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalFonts = createGlobalStyle`
+      @font-face {
+            font-family: '';
+            src: url() format('woff');
+      }
+`;
