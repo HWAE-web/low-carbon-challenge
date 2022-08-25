b0bca1afa5c9baf9647b26501d874dd25c6a1899
Haewonny <hawon2205@ewhain.net>
2022-08-26 00:57:07 +0900

    feat: add MapPage marker&data

--- src/Components/MapPage/MapPage.js
+++ src/Components/MapPage/MapPage.js
@@ -1,22 +1,128 @@
-import NavBar from "../NavBar/NavBar";
-import { MapPageContainer } from "./MapPageElement";
+/* global kakao */
+import { Column1, Column2, Desc, MapCol, MapPageContainer, MapPageWrapper, MapRow,Plus, Minus, R1, R2, Row1, Row2, Title, TitleCol } from "./MapPageElement";
+import { useEffect, useState, useRef } from "react";
+import { GlobalFonts } from "../../fonts/font";
+import { Map, MapMarker, ZoomControl, CustomOverlayMap } from "react-kakao-maps-sdk";
+import "./MapPage.css";
+import plus from "../../images/plus.svg";
+import minus from "../../images/minus.svg";
+import { cafe_data } from "./data";
+import { restaurant_data } from "./data";
+import { shop_data } from "./data";
 
 function MapPage(){
+    let [cafeData] = useState(cafe_data); 
+    let [restaurantData] = useState(restaurant_data); 
+    let [shopData] = useState(shop_data); 
     return(
         <>
-        <NavBar/>
         <MapPageContainer>
-        {/* <Map/> */}
+        <GlobalFonts/>
+        <MapPageWrapper>
+            <MapCol>
+              <Row1>
+                <TitleCol>
+                    <R1>
+                        <Title>우리동네 저탄소 지도</Title>
+                    </R1>
+                    <R2>
+                        <Desc>집 근처 가까운 곳에서 저탄소 챌린지를 실천할 수 있는 장소들을 찾아보세요!</Desc>
+                    </R2>
+                </TitleCol>
+              </Row1>
+              <Row2>
+                <MapRow>
+                    <Column1>
+                        <MapAPI cafeData={cafeData} restaurantData={restaurantData} shopData={shopData}/>
+                    </Column1>
+                    <Column2>
+                    </Column2>
+                </MapRow>
+              </Row2>
+            </MapCol>
+        </MapPageWrapper>
         </MapPageContainer>
         </>
     )
 }
 
-export function Map(){
-    return(
-        <>
-        <div>지도 api</div>
-        </>
-    )
-}
 export default MapPage;
+
+export function MapAPI(props){
+    const mapRef = useRef();
+
+    const zoomIn = () => {
+        const map = mapRef.current
+        map.setLevel(map.getLevel() - 1)
+    }
+
+    const zoomOut = () => {
+        const map = mapRef.current
+        map.setLevel(map.getLevel() + 1)
+    }
+
+  return (
+    <>
+    <div className={`map_wrap`}>
+    <Map
+      center={props.cafeData[1].latlng }
+      style={{ width: "100%",
+      height: "100%",
+      position: "relative",
+      overflow: "hidden", }}
+      level={2}
+      draggable={true} // 지도 드래그 true/false
+      ref={mapRef}
+    >
+        {props.cafeData.map((a, index) => (
+        <MapMarker
+          key={`${props.cafeData[index].name}-${props.cafeData[index].latlng}`}
+          position={props.cafeData[index].latlng} // 마커를 표시할 위치
+          image={{
+            src: "https://ifh.cc/g/cmo6Ta.png", // 마커이미지 주소
+            size: {
+              width: 45,
+              height: 55
+            }, // 마커이미지의 크기
+          }}
+        />))}
+
+        {props.restaurantData.map((a, index) => (
+        <MapMarker
+          key={`${props.restaurantData[index].name}-${props.restaurantData[index].latlng}`}
+          position={props.restaurantData[index].latlng} // 마커를 표시할 위치
+          image={{
+            src: "https://ifh.cc/g/5LH8yK.png", // 마커이미지 주소
+            size: {
+              width: 45,
+              height: 55
+            }, // 마커이미지의 크기
+          }}
+        />))}
+
+        {props.shopData.map((a, index) => (
+        <MapMarker
+          key={`${props.shopData[index].name}-${props.shopData[index].latlng}`}
+          position={props.shopData[index].latlng} // 마커를 표시할 위치
+          image={{
+            src: "https://ifh.cc/g/6QCpH8.png", // 마커이미지 주소
+            size: {
+              width: 45,
+              height: 55
+            }, // 마커이미지의 크기
+          }}
+        />))}
+   
+    </Map>
+    <div className="custom_zoomcontrol radius_border"> 
+            <span onClick={zoomIn}>
+              <Plus src={plus} alt="plus"/>
+            </span>
+            <span onClick={zoomOut}>
+              <Minus src={minus} alt="minus"/>
+            </span>
+        </div>
+    </div>
+    </>
+  )
+}
--- src/Components/MapPage/MapPageElement.js
+++ src/Components/MapPage/MapPageElement.js
@@ -6,3 +6,135 @@ export const MapPageContainer = styled.div`
     height: 100vh;
     width: 100vw;
 `
+export const MapPageWrapper = styled.div`
+    display: grid;
+    z-index: 3;
+    height: 90%;
+    width: 100%;
+    margin: 100px 80px 0 80px;
+    @media screen and (max-width: 1024px) {
+        
+    }
+    @media screen and (max-width: 768px) {
+        
+    }
+`;
+
+export const MapCol = styled.div`
+    display: grid;
+    grid-template-rows: 100px 110px 110px 110px 110px 110px 110px 110px;
+    grid-template-areas: "row1" "row2" "row2" "row2" "row2" "row2" "row2" "row2"; 
+
+    @media screen and (max-width: 1024px) {
+        
+    }
+    @media screen and (max-width: 768px) {
+        
+    }
+`;
+
+export const Row1 = styled.div`
+    grid-area: row1;
+    @media screen and (max-width: 1024px) {
+        
+    }
+    @media screen and (max-width: 768px) {
+        
+    }
+`;
+
+export const Row2 = styled.div`
+    grid-area: row2;
+    
+`;
+
+export const TitleCol = styled.div`
+    display: grid;
+    grid-template-rows: 60px 50px;
+    grid-template-areas: "r1" "r2"; 
+
+    @media screen and (max-width: 1024px) {
+        
+    }
+    @media screen and (max-width: 768px) {
+        
+    }
+`;
+
+export const R1 = styled.div`
+    grid-area: r1;
+    @media screen and (max-width: 1024px) {
+        
+    }
+    @media screen and (max-width: 768px) {
+        
+    }
+`;
+
+export const R2 = styled.div`
+    grid-area: r2;
+    
+`;
+
+export const Title = styled.div`
+    max-width: 500px;
+    margin: 0 0 0 20px;
+    font-family: "NotoSans Bold";
+    font-style: normal;
+    font-weight: 900;
+    font-size: 45px;
+    line-height: 56px;
+    letter-spacing: 0.2px;
+ 
+    color: #424242;
+`
+
+export const Desc = styled.div`
+    max-width: 564px;
+    margin: -5px 0 0 25px;
+    font-family: "NotoSans Regular";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 17px;
+    line-height: 56px;
+    letter-spacing: 0.2px;
+ 
+    color: #767676;
+`
+
+export const MapRow = styled.div`
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    align-items: center; 
+    margin: 0;
+
+    grid-template-areas: "col1 col1 col1 col1 col1 col2 col2";
+
+    @media screen and (max-width: 1024px) {
+        
+    }
+    @media screen and (max-width: 768px) {
+        
+    }
+`;
+export const Column1 = styled.div`
+  grid-area: col1;
+  margin: 10px 0 0 20px;
+  @media screen and (max-width: 768px) {
+    padding: 0;
+ }
+`;
+
+export const Column2 = styled.div`
+  grid-area: col2;
+  @media screen and (max-width: 768px) {
+    padding: 0;
+ }
+`;
+
+export const Plus = styled.img`
+    max-width: 30px;
+`
+export const Minus = styled.img`
+    max-width: 30px;
+`
