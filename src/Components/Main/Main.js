99ddf25e39eacf05c54f6c3ba09e303edc5d91c6
yonsoo <ynsoo.seo@gmail.com>
2022-08-28 17:59:46 +0900

    test

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,15 +1,14 @@
 import { GlobalFonts } from "../../fonts/font";
 import { MainContainer } from "./MainElement";
 
-function Main(){
-    return(
-        <>
-        <GlobalFonts/>
-        <MainContainer>
-
-        </MainContainer>
-        </>
-    )
+function Main() {
+  return (
+    <>
+      <GlobalFonts />
+      <MainContainer></MainContainer>
+      <></>
+    </>
+  );
 }
 
 export default Main;
