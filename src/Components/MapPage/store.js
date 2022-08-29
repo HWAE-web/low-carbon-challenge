f6fb298f16fa45f04d4a000ef909a672dd3a43b0
Haewonny <hawon2205@ewhain.net>
2022-08-29 21:24:59 +0900

    feat: use redux

--- src/Components/MapPage/store.js
+++ src/Components/MapPage/store.js
@@ -1,7 +1,24 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit';
 
+let detail = createSlice({
+    name: "detail",
+    initialState: [
+      
+    ],
+    reducers: {
+      addState(state, action){
+        state.push(action.payload);
+      },
+      clearState(state, action){
+        state.pop(action.payload);
+      }
+    },
+  });
+  
+export let { addState, clearState } = detail.actions;
+
 export default configureStore({
     reducer:{
-
+      detail : detail.reducer
     }
 })
--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import store from './Components/MapPage/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <GlobalFonts />
         <App />
       </BrowserRouter>
     </Provider>    
