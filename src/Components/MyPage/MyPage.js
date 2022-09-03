54f922965e0a67b9fc65250a9fd1879351aa8a1f
Haewonny <hawon2205@ewhain.net>
2022-09-04 01:34:08 +0900

    feat: MyPage

--- src/Components/MyPage/MyPage.js
+++ src/Components/MyPage/MyPage.js
@@ -1,15 +1,57 @@
 import { GlobalFonts } from "../../fonts/font";
-import { MyPageContainer } from "./MyPageElement";
+import {
+  Column1,
+  Column2,
+  Desc,
+  ImgBG,
+  MyPageArea,
+  MyPageBG,
+  MyPageContainer,
+  MyPageRow,
+  NameArea,
+  PlaceName,
+  Profile,
+  RecentArea,
+} from "./MyPageElement";
+import mypageBG from "../../images/mypageBG.svg";
+import profile from "../../images/profile.svg";
+import { useSelector, useDispatch } from "react-redux";
 
-function MyPage(){
-    return(
-        <>
-        <GlobalFonts/>
-        <MyPageContainer>
-            
-        </MyPageContainer>
-        </>
-    )
+function MyPage() {
+    // redux state
+    let state = useSelector((state) => {
+      return state;
+    });
+  return (
+    <>
+      <GlobalFonts />
+      <MyPageContainer>
+        <MyPageBG>
+          <ImgBG src={mypageBG} alt="BG" />
+        </MyPageBG>
+        <MyPageArea>
+          <MyPageRow>
+            <Column1>
+              <Profile src={profile} alt="profile"/>
+              <NameArea>이탄소 님 {'>'}</NameArea>
+              <Desc>지금까지 6881 마일리지가 쌓였어요!</Desc>
+              <RecentArea>나의 최근 관심 장소</RecentArea>
+              {state.detail.map((a, i) => (
+                      <div key={i}>
+                        <PlaceName as="a" href={state.detail[i].href}>{state.detail[i].name}</PlaceName>
+                      </div>
+                    ))}
+              
+            </Column1>
+            <Column2>
+           
+              
+            </Column2>
+          </MyPageRow>
+        </MyPageArea>
+      </MyPageContainer>
+    </>
+  );
 }
 
 export default MyPage;
